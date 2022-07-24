b6bbf6033cfbcfd7f0886a49868d67939dda29a3
Ajay-2007 <iit2017040@iiita.ac.in>
2022-07-25 00:17:22 +0530

    added rest of the django basics project files

--- Django-Projects/django4/django4_basics/my_site_1/my_app/views.py
+++ Django-Projects/django4/django4_basics/my_site_1/my_app/views.py
@@ -6,13 +6,6 @@ def example_view(request):
     return render(request, 'my_app/example.html')
 
 def variable_view(request):
-
-    my_var = {'first_name': 'rosalind', 'last_name': 'Franklin',
     
-        'some_list': [1, 2, 3],
-        'some_dict': {'inside_key': 'inside_value'},
-        'user_logged_in': True,
-    }
 
-
-    return render(request, 'my_app/variable.html', context=my_var)
+    return render(request, 'my_app/variable.html')
--- Django-Projects/django4/django4_basics/my_site_1/my_site_1/settings.py
+++ Django-Projects/django4/django4_basics/my_site_1/my_site_1/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-xq$lnxlo$*ytl)mv%mlh+l8sefwbn21ulqwgo#1+1qq@o_6z9#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'my_site_1.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,6 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+# my_app/static/my_app/django.jpg
 STATIC_URL = 'static/'
 
 # Default primary key field type
