3429a5f1942194ab94146a17126401c8c0ec2579
Ajay-2007 <iit2017040@iiita.ac.in>
2022-07-26 19:54:45 +0530

    completed login and logout

--- Django-Projects/full_stack_social_media_web_app/src/account/forms.py
+++ Django-Projects/full_stack_social_media_web_app/src/account/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, login
 
 from account.models import Account
 
@@ -32,6 +32,31 @@ class RegistrationForm(UserCreationForm):
             account = Account.objects.get(username=username)
         except Exception as e:
             return username
-            
+
         raise forms.ValidationError(f"Email {username} is already in use.")
 
+
+class AccountAuthenticationForm(forms.ModelForm):
+
+    email = forms.EmailField(label="Email address", widget=forms.EmailInput)
+    # we are using forms.PasswordInput widget so that the password field is hidden
+    password = forms.CharField(label="Password", widget=forms.PasswordInput)
+
+    class Meta:
+        model = Account
+        fields = ("email", "password")
+
+    # def save(self):    
+    #     email = self.cleaned_data['email']
+    #     password = self.cleaned_data['password']
+
+    #     user = authenticate(email=email, password=password)
+    #     if user:
+    #         login(self.request, user)
+
+    def clean(self):
+        if self.is_valid():
+            email = self.cleaned_data['email']
+            password = self.cleaned_data['password']
+            if not authenticate(email=email, password=password):
+                raise forms.ValidationError("Invalid Login")
