380c47165d7e3e020ca261427038ad5c42c1cd25
Ajay-2007 <iit2017040@iiita.ac.in>
2022-07-27 18:58:57 +0530

    completed accept friend request

--- Django-Projects/full_stack_social_media_web_app/src/friend/urls.py
+++ Django-Projects/full_stack_social_media_web_app/src/friend/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 
 from friend.views import (
     send_friend_request,
-    friend_requests
+    friend_requests,
+    accept_friend_request
 )
 
 
@@ -11,5 +12,6 @@ app_name = "friend"
 urlpatterns = [
     path('friend_request/', send_friend_request, name='friend-request'),
     path('friend_request/<user_id>', friend_requests, name='friend-requests'),
+    path('friend_request_request/<friend_request_id>', accept_friend_request, name='friend-request-accept'),
 
 ]
